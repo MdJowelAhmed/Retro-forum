89d2d09b0b7564938223657fefa4f438cbd2f7a5
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-03-03 00:55:32 +0600

    added somethings functionality

---
+++ script/retro.js
@@ -0,0 +1,54 @@
+ const loadDiscussData=async()=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+    const data=await res.json()
+    const allData=data.posts
+    // console.log(allData)
+    const cardContainer=document.getElementById('card-container')
+    allData.forEach((singleData)=>{
+        console.log(singleData)
+        const div=document.createElement('div')
+        div.innerHTML=`
+        <div class="w-2/3 flex gap-6 p-10 bg-[#797DFC] space-y-3 rounded-2xl">
+          <img class="h-full" src="image/Rectangle 4.png" alt="">
+          <div class="">
+            <div class="flex">
+              <p class="mr-10 lg:mr-32">#music</p>
+              <p>author:Awolad hossen</p>
+            </div>
+            <div>
+              <h2 class="text-2xl font-bold">10 kit title</h2>
+            </div>
+            <div class="mb-5">
+              <p>It's one things to subjectss </p>
+            </div>
+            <hr>
+            
+              <div>
+                <div  class="flex mt-5 justify-between">
+                  <div class="flex gap-2">
+                    <img src="image/Group 13.png" alt="">
+                    <p>560</p>
+                  </div>
+                  <div class="flex gap-2">
+                    <img src="image/Group 16.png" alt="">
+                    <p>560</p>
+                  </div>
+                  <div class="flex gap-2">
+                    <img src="image/Group 18.png" alt="">
+                    <p>560</p>
+                  </div>
+                  <div>
+                    <img src="image/Group 40106.png" alt="">
+                   </div>
+                </div>
+               
+             
+             
+            </div>
+          </div>
+        </div>
+        `;
+        cardContainer.appendChild(div)
+    })
+ }
+ loadDiscussData()
